0875988c146290357c0c9c7dc43d56cc58b56385
ADEEB09 <145766356+ADEEB09@users.noreply.github.com>
2023-11-09 19:46:04 +0530

    REACT_TASK_3

--- App.js
+++ App.js
@@ -1,122 +1,65 @@
-import React, { Component, useEffect, useState } from 'react'
-import './App.css';
-import Header from "./components/header/Header"
-import Keypad from "./components/keypad/Keypad"
-import moon from "./images/moon.png"
-import sun from "./images/sun.png"
-
-const usekeycode = [
-  48,49,50,51,52,53,54,55,56,57,96,97,98,99,100,101,102,103,104,105,8,13,190,187,189,191,56,111,106,107,109
-];
-
-const numbers = [
-  "0","1","2","3","4","5","6","7","8","9"
-];
-
-const operators = [
-  "*" , "+","-","/"
-];
-
-
+import { SketchPicker, BlockPicker } from "react-color";
+import { useState } from "react";
 
 function App() {
-  const [isdark , setisdark] = useState(JSON.parse(localStorage.getItem("calculator-app-mode")) || false);
+  //creating state to store our color and also set color using onChange event for sketch picker
+  const [sketchPickerColor, setSketchPickerColor] = useState({
+    r: "241",
+    g: "112",
+    b: "19",
+    a: "1",
+  });
+  // destructuring rgba from state
+  const { r, g, b, a } = sketchPickerColor;
 
-  const [exp , setexp] = useState("");
-  const  [result , setresult] = useState("");
-  const[history , sethistory] = useState(JSON.parse(
-    localStorage.getItem("calculator-app-mode-history")) || []
-  );
-
-  const handleKeyPress = (keycode , key) => {
-    if(!keycode)return;
-    if(!usekeycode.includes(keycode))return;
-
-
-    if(numbers.includes(key)){
-      if(key == "0"){
-        if(exp.length === 0)return;
-      }
-      calculator(exp + key);
-      setexp(exp + key);
-    }
-    else if(operators.includes(key)){ 
-      if(!exp)return
-      const lastchar = exp.slice(-1);
-      if(operators.includes(lastchar) || lastchar === ".")return;
-      setexp(exp + key);
-    }
-    else if(key === "."){
-      if(!exp)return
-      const lastchar = exp.slice(-1);
-      if(!numbers.includes(lastchar))return;
-      setexp(exp + ".");
-    }
-    else if(keycode===8){
-      if(!exp){
-        setresult("");
-        return;
-      }
-      calculator(exp.slice(0,-1));
-      setexp(exp.slice(0,-1));
-    }
-    else if(keycode===13){
-      if(!exp)return;
-      calculator(exp);
-      const temphistory = [...history]
-      if(temphistory.length > 20){
-        temphistory = temphistory.slice(0,1);
-      }
-      temphistory.push(exp);
-      sethistory(temphistory);
-    }
-  }
-
-  const calculator = (exp) => {
-    if(!exp){
-      setresult("");
-      return;
-    }
-    const lastchar = exp.slice(-1);
-    if(!numbers.includes(lastchar))exp=exp.slice(0,1);
-    const answer = eval(exp).toFixed(2) + "";
-    console.log(answer);
-    setresult(answer);
-    console.log(result);
-  }
-
-  useEffect( () => {
-    localStorage.setItem("calculator-app-mode-history" , JSON.stringify(history))
-  } , [history])
-
-  useEffect( () => {
-    localStorage.setItem("calculator-app-mode" , JSON.stringify(isdark))
-  } , [isdark])
+  //creating state to store our color and also set color using onChange event for block picker
+  const [blockPickerColor, setBlockPickerColor] = useState("#37d67a");
 
   return (
-    <div className="app" data-theme={isdark ? "dark" : ""}
-          tabIndex="0"
-          onKeyDown={(event) => handleKeyPress(event.keyCode , event.key)}
+    <div
+      className="App"
+      style={{ display: "flex", justifyContent: "space-around" }}
     >
-      <div className='app_calculator'>
-        <div className='app_calculator_navbar'>
-          <div className='app_calculator_navbar_toggle' 
-          onClick={() => setisdark(!isdark)}>
-            <div 
-            className={`app_calculator_navbar_toggle_circle  ${
-              isdark ? "app_calculator_navbar_toggle_circle_active" : ""
-            }`}
-            />
-            </div>
-            <img src= {isdark ? moon : sun} alt="mode" />
-        </div>
-
-        <Header exp={exp}  result={result}  history = {history}/>
-        <Keypad handleKeyPress={handleKeyPress}  />
+      <div className="sketchpicker">
+        <h6>Sketch Picker</h6>
+        {/* Div to display the color  */}
+        <div
+          style={{
+            backgroundColor: `rgba(${r},${g},${b},${a})`,
+            width: 100,
+            height: 50,
+            border: "2px solid white",
+          }}
+        ></div>
+        {/* Sketch Picker from react-color and handling color on onChange event */}
+        <SketchPicker
+          onChange={(color) => {
+            setSketchPickerColor(color.rgb);
+          }}
+          color={sketchPickerColor}
+        />
+      </div>
+      <div className="blockpicker">
+        <h6>Color Picker</h6>
+        {/* Div to display the color  */}
+        <div
+          style={{
+            backgroundColor: `${blockPickerColor}`,
+            width: 100,
+            height: 50,
+            border: "2px solid white",
+          }}
+        ></div>
+        {/* Block Picker from react-color and handling color on onChange event */}
+        <BlockPicker
+          color={blockPickerColor}
+          onChange={(color) => {
+            setBlockPickerColor(color.hex);
+          }}
+        />
       </div>
     </div>
-  )
+  );
 }
 
 export default App;
-
