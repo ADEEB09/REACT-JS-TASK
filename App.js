7d704b22f390cbb5e8fc955468a94d37cc77f5ff
ADEEB09 <145766356+ADEEB09@users.noreply.github.com>
2023-11-09 19:40:42 +0530

    REACT_TASK_2

--- App.js
+++ App.js
@@ -1,47 +1,122 @@
-import { useState, useEffect } from 'react';
+import React, { Component, useEffect, useState } from 'react'
 import './App.css';
+import Header from "./components/header/Header"
+import Keypad from "./components/keypad/Keypad"
+import moon from "./images/moon.png"
+import sun from "./images/sun.png"
+
+const usekeycode = [
+  48,49,50,51,52,53,54,55,56,57,96,97,98,99,100,101,102,103,104,105,8,13,190,187,189,191,56,111,106,107,109
+];
+
+const numbers = [
+  "0","1","2","3","4","5","6","7","8","9"
+];
+
+const operators = [
+  "*" , "+","-","/"
+];
+
+
 
 function App() {
-  const [text, setText] = useState('');
+  const [isdark , setisdark] = useState(JSON.parse(localStorage.getItem("calculator-app-mode")) || false);
 
-  const [wordCount, setWordCount] = useState(0);
+  const [exp , setexp] = useState("");
+  const  [result , setresult] = useState("");
+  const[history , sethistory] = useState(JSON.parse(
+    localStorage.getItem("calculator-app-mode-history")) || []
+  );
 
-  const [charCount, setCharCount] = useState(0);
+  const handleKeyPress = (keycode , key) => {
+    if(!keycode)return;
+    if(!usekeycode.includes(keycode))return;
 
-  const changeHandler = (event) => {
-    setText(event.target.value);
-  };
 
-  useEffect(() => {
-    const words = text.split(' ');
-
-    let wordCount = 0;
-    words.forEach((word) => {
-      if (word.trim() !== '') {
-        wordCount++;
+    if(numbers.includes(key)){
+      if(key == "0"){
+        if(exp.length === 0)return;
       }
-    });
-    setWordCount(wordCount);
+      calculator(exp + key);
+      setexp(exp + key);
+    }
+    else if(operators.includes(key)){ 
+      if(!exp)return
+      const lastchar = exp.slice(-1);
+      if(operators.includes(lastchar) || lastchar === ".")return;
+      setexp(exp + key);
+    }
+    else if(key === "."){
+      if(!exp)return
+      const lastchar = exp.slice(-1);
+      if(!numbers.includes(lastchar))return;
+      setexp(exp + ".");
+    }
+    else if(keycode===8){
+      if(!exp){
+        setresult("");
+        return;
+      }
+      calculator(exp.slice(0,-1));
+      setexp(exp.slice(0,-1));
+    }
+    else if(keycode===13){
+      if(!exp)return;
+      calculator(exp);
+      const temphistory = [...history]
+      if(temphistory.length > 20){
+        temphistory = temphistory.slice(0,1);
+      }
+      temphistory.push(exp);
+      sethistory(temphistory);
+    }
+  }
 
-    setCharCount(text.length);
-  }, [text]);
+  const calculator = (exp) => {
+    if(!exp){
+      setresult("");
+      return;
+    }
+    const lastchar = exp.slice(-1);
+    if(!numbers.includes(lastchar))exp=exp.slice(0,1);
+    const answer = eval(exp).toFixed(2) + "";
+    console.log(answer);
+    setresult(answer);
+    console.log(result);
+  }
+
+  useEffect( () => {
+    localStorage.setItem("calculator-app-mode-history" , JSON.stringify(history))
+  } , [history])
+
+  useEffect( () => {
+    localStorage.setItem("calculator-app-mode" , JSON.stringify(isdark))
+  } , [isdark])
 
   return (
-    <div className='container'>
-      <h2>Responsive Paragraph Word Counter</h2>
-      <div>
-        <textarea
-          value={text}
-          onChange={changeHandler}
-          placeholder='What is on your mind?'
-        ></textarea>
-
-        <div>
-          <p className='word-count'>Word Count: {wordCount}</p>
+    <div className="app" data-theme={isdark ? "dark" : ""}
+          tabIndex="0"
+          onKeyDown={(event) => handleKeyPress(event.keyCode , event.key)}
+    >
+      <div className='app_calculator'>
+        <div className='app_calculator_navbar'>
+          <div className='app_calculator_navbar_toggle' 
+          onClick={() => setisdark(!isdark)}>
+            <div 
+            className={`app_calculator_navbar_toggle_circle  ${
+              isdark ? "app_calculator_navbar_toggle_circle_active" : ""
+            }`}
+            />
+            </div>
+            <img src= {isdark ? moon : sun} alt="mode" />
         </div>
+
+        <Header exp={exp}  result={result}  history = {history}/>
+        <Keypad handleKeyPress={handleKeyPress}  />
       </div>
     </div>
-  );
+  )
 }
 
 export default App;
+
