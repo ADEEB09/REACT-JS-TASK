a4c708aeedb22d8b32720e347a8e345473b1d9c3
ADEEB09 <145766356+ADEEB09@users.noreply.github.com>
2023-11-09 19:48:18 +0530

    REACT_TASK_5

--- App.js
+++ App.js
@@ -1,67 +1,30 @@
-import { useEffect, useState } from "react";
-import axios from "axios"; // Import Axios
+import React, { useState } from "react";
+import "./index.css";
 
-function App() {
-  const [users, setUsers] = useState([]);
-  const [loading, setLoading] = useState(false);
+export default function App() {
+  var currentYear = 2023;
+  const [birthYear, setbirthYear] = useState(0);
+  const [age, setAge] = useState(0);
 
-  useEffect(() => {
-    setLoading(true);
-    axios
-      .get("https://dummyjson.com/users") // Use Axios for the GET request
-      .then(response => {
-        setUsers(response.data); // Axios response data is in response.data
-      })
-      .catch(error => {
-        console.error("Axios error:", error);
-      })
-      .finally(() => {
-        setLoading(false);
-      });
-  }, []);
+  function clickHandler() {
+    var theAge = currentYear - birthYear;
+    setAge(theAge);
+  }
+
+  function inputChangeHandler(e) {
+    setbirthYear(e);
+  }
 
   return (
     <div className="App">
-      {loading ? (
-        <div>Loading...</div>
-      ) : (
-        <>
-          <h1>Dummy Data</h1>
-          <table border={1}>
-            <thead>
-              <tr>
-                <th>Sno</th>
-                <th>Profile Pic</th>
-                <th>First Name</th>
-                <th>Last Name</th>
-                <th>Gender</th>
-                <th>email</th>
-                <th>Username</th>
-                <th>Phone</th>
-                <th>IP</th>
-                <th>University</th>
-              </tr>
-            </thead>
-            <tbody>
-              {users.map(user => (
-                <tr key={user.id}>
-                  <td>{user.id}</td>
-                  <td>{user.image}</td>
-                  <td>{user.firstName}</td>
-                  <td>{user.lastName}</td>
-                  <td>{user.gender}</td>
-                  <td>{user.username}</td>
-                  <td>{user.phone}</td>
-                  <td>{user.ip}</td>
-                  <td>{user.university}</td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
-        </>
-      )}
+      <h1>Age Calculator</h1>
+      <input onChange={(e) => inputChangeHandler(e.target.value)} /><br/>
+      <div>
+        {" "}
+        <button onClick={clickHandler}>Submit</button>
+      </div>
+
+      <div>The age is: {age}</div>
     </div>
   );
 }
-
-export default App;
